80932b26f946832f900f8cbc08704a964c5ea228
Admin <Admin@ADMIN>
2021-12-17 16:03:53 +0700

    Add project files.

---
+++ WebApp1/Pages/Privacy.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authorization;
+
+namespace WebApp1.Pages
+{
+    [Authorize]
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
